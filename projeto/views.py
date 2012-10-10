67687ba842b0778ba8cadaea76f5f6e8ebc5a5cc
Augusto das Neves <augustodasneves@hotmail.com>
2012-10-10 09:32:29 -0300

    Criado método generico ciraçao de forms de adição e edição

--- projeto/views.py
+++ projeto/views.py
@@ -1,48 +1,30 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from models import Documento,Tarefa,statusTarefa
-from projeto_forms.formsTarefa import formDocumento,formStatusTarefa
+from django.test._doctest import _OutputRedirectingPdb
+from models import *
+from projeto_forms.formsTarefa import *
 
 def index(request):
     return HttpResponse("teste")
 
 '''TAREFA'''
 def adicionar(request,nameForm):
+    try:
+        nameForm=eval('form'+nameForm)
+    except:
+        raise Http404
+
     if request.method=="POST":
-        form=formStatusTarefa(request.POST,request.FILES)
-
+        form=nameForm(request.POST,request.FILES)
         if form.is_valid():
-            dadosStatusTarefa=form.cleaned_data
-
-            objStatusTarefa=statusTarefa(nome=dadosStatusTarefa['nome'])
-            objStatusTarefa.save()
+            form.save()
 
             return render_to_response('salvo.html',{})
     else:
-        form=formStatusTarefa()
+        form=nameForm()
 
-    return render_to_response('adicionar.html',{'form':form})
+    return render_to_response('adicionar.html',{'form':form},context_instance=RequestContext(request))
 
 def excluirTarefa(request,id_tarefa):
     return HttpResponse()
-
-'''DOCUMENTO'''
-def adicionarDocumento(request):
-    if request.method=="POST":
-        formulario=formDocumento(request.POST,request.FILES)
-        if formulario.is_valid():
-            dadosDocumento=formulario.cleaned_data
-
-            documento=Documento(
-                tarefa=Tarefa.objects.get(pk=dadosDocumento['id_tarefa']),
-                nome=dadosDocumento['nome'],
-                caminho=dadosDocumento['caminho']
-            )
-            documento.save()
-
-            return render_to_response('salvo.html',{})
-    else:
-        formulario=formDocumento()
-
-    return render_to_response('adicionar.html',{'form':formulario},context_instance=RequestContext(request))
